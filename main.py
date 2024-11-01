b7a9a89e63b456f8c57886f308cde90858ba5ee3
jcoffeex <johnander.dev@gmail.com>
2024-11-01 18:32:17 -0300

    fix: problem with CORS

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from flask import Flask, render_template, request, jsonify, redirect
+from flask_cors import CORS
 import sqlite3
 import uuid
 app = Flask(__name__)
-
+CORS(app)
 def get_db_connection():
     conn = sqlite3.connect('url_shortener.db') 
     conn.row_factory = sqlite3.Row 
